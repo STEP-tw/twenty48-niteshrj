736a5f863faec5f228560291f2c5a816825e9d3a
Nitesh Ranjan <>
2019-02-22 18:34:30 +0530

    Implement move-grid-left, down and up.

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -3,6 +3,8 @@
 
 (def remove-zeros (partial filter (comp not zero?)))
 
+(def transpose (partial apply mapv vector))
+
 (def add-consecutive-equal-numbers
   (comp
    reverse
@@ -11,27 +13,14 @@
    reverse
    (partial partition-by identity)))
 
-(def append-zeros (comp (partial take-last 4) flatten (partial cons '(0 0 0 0))))
+(def append-zeros (comp (partial take-last 4) flatten (partial cons (repeat 4 0))))
 
 (def move (comp append-zeros add-consecutive-equal-numbers remove-zeros))
 
-;----------------------------------------------------------------------------------------------
+(def move-grid-right "Moves an entire grid to the right" (partial map move ))
 
-(def move-grid-right
-  "Moves an entire grid to the right"
-  (partial map move ))
+(def move-grid-left "Moves an entire grid to the left" (comp (partial map reverse) (partial map move) (partial map reverse)))
 
-(defn move-grid-left
-  "Moves an entire grid to the left"
-  [grid]
-  grid)
+(def move-grid-down "Moves an entire grid down" (comp (partial transpose) (partial map move) (partial transpose)))
 
-(defn move-grid-down
-  "Moves an entire grid down"
-  [grid]
-  grid)
-
-(defn move-grid-up
-  "Moves an entire grid up"
-  [grid]
-  grid)
+(def move-grid-up "Moves an entire grid up" (comp (partial transpose) (partial move-grid-left) (partial transpose)))
