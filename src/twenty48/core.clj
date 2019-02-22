06bf9ddc9a8c3adfbb393df0852fe9eb981c8fc4
Nitesh Ranjan <>
2019-02-22 11:45:19 +0530

    Implement move-grid-right feature.

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,10 +1,25 @@
 (ns twenty48.core
   (:gen-class))
 
-(defn move-grid-right
+(def remove-zeros (partial filter (comp not zero?)))
+
+(def add-consecutive-equal-numbers
+  (comp
+   reverse
+   (partial map (comp (partial apply +) (partial take-last 2)))
+   (partial mapcat (partial partition-all 2))
+   reverse
+   (partial partition-by identity)))
+
+(def append-zeros (comp (partial take-last 4) flatten (partial cons '(0 0 0 0))))
+
+(def move (comp append-zeros add-consecutive-equal-numbers remove-zeros))
+
+;----------------------------------------------------------------------------------------------
+
+(def move-grid-right
   "Moves an entire grid to the right"
-  [grid]
-  grid)
+  (partial map move ))
 
 (defn move-grid-left
   "Moves an entire grid to the left"
